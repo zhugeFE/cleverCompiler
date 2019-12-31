553bd754662a48d8ba53346f43d805473249f1e7
张荣 <zhangrong@zhugeio.com>
2019-12-31 18:55:41 +0800

    no message

--- src/modules/config/panels/projectTabs.vue
+++ src/modules/config/panels/projectTabs.vue
@@ -3,12 +3,13 @@
     <el-tabs v-model="active" @tab-click="onChosen" @tab-remove="onDelete" @tab-add="onAdd" tab-position="bottom" type="card" :closable="true" :addable="true">
       <el-tab-pane v-for="item in list" :key="item.id" :label="item.name" :name="item.name">
         <div class="project-content">
-          这是项目配置
+          这是项目{{item.name}}的配置
         </div>
       </el-tab-pane>
     </el-tabs>
     <el-dialog :visible.sync="showDialog">
-      <div>选择项目</div>
+      <div>选择项目1？</div>
+      <el-button @click="onsubmit" type="primary">确定</el-button>
     </el-dialog>
   </div>
 </template>
@@ -73,6 +74,16 @@
       onAdd(data) {
         this.showDialog = true
         console.log('add: ', data)
+      },
+      onsubmit() {
+        let source = this.idMap['1']
+        this.list.push({
+          id: source.id,
+          name: source.name,
+          version_id: null,
+          buildMod: 'downLoad'
+        })
+        this.showDialog = false
       },
       onDelete(data) {
         console.log('del: ', data)
