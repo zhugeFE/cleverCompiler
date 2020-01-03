fdfa79460b005b96deda8ba3a176e70cb6d55a29
张荣 <zhangrong@zhugeio.com>
2020-01-03 17:31:17 +0800

    no message

--- src/modules/compile/action.vue
+++ src/modules/compile/action.vue
@@ -1,15 +1,71 @@
 <template>
   <div class="compile-action">
-    编译
+    <div class="line">
+      <label class="title">名称</label>
+      <div class="content">
+        <el-input v-model="name"></el-input>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">发布方式</label>
+      <div class="content">
+        <el-checkbox v-model="publishingMode.git">发布到git</el-checkbox>
+        <el-checkbox v-model="publishingMode.download">打包下载</el-checkbox>
+        <el-checkbox v-model="publishingMode.auto">自动</el-checkbox>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">描述</label>
+      <div class="content">
+        <el-input
+            style="width: 350px;"
+            type="textarea"
+            :rows="3"
+            placeholder="请输入内容"
+            v-model="describe">
+        </el-input>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title"></label>
+      <div class="content">
+        <el-button>编译</el-button>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title"></label>
+      <div class="content">
+        <div class="logs" v-show="logList.length">
+          <div class="title">日志信息</div>
+          <el-button size="mini" icon="el-icon-rank" class="showAll">全屏显示</el-button>
+          <ul class="content" ref="logs">
+            <li v-for="(log, index) in logList" :class="[log.tag]" :key="log.msg + index">{{log.msg}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'compileAction'
+    name: 'compileAction',
+    data() {
+      return {
+        name: '',
+        publishingMode: {
+          git: false,
+          download: false,
+          auto: true
+        },
+        describe: '',
+        logList: []
+
+      }
+    }
   }
 </script>
 
 <style lang="sass">
-
+@import "./styles/action"
 </style>
--- src/modules/compile/info.vue
+++ src/modules/compile/info.vue
@@ -9,7 +9,7 @@
     <div class="line">
       <label class="title">模版</label>
       <div class="content">
-        <el-select v-model="config" filterable placeholder="请选择">
+        <el-select v-model="config" filterable placeholder="套件列表">
           <el-option
               v-for="item in configList"
               :key="item.id"
@@ -17,7 +17,80 @@
               :value="item.id">
           </el-option>
         </el-select>
+        <el-select v-model="version" filterable placeholder="版本列表" style="margin-left: 10px">
+          <el-option
+              v-for="item in versionList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
+          </el-option>
+        </el-select>
       </div>
+    </div>
+    <div class="line">
+      <label class="title">编译类型</label>
+      <div class="content">
+        <el-select v-model="type" filterable placeholder="编译类型">
+          <el-option
+              v-for="item in publicTypes"
+              :key="item"
+              :label="item"
+              :value="item">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">发布方式</label>
+      <div class="content">
+        <el-checkbox v-model="publishingMode.git">发布到git</el-checkbox>
+        <el-checkbox v-model="publishingMode.download">打包下载</el-checkbox>
+        <el-checkbox v-model="publishingMode.auto">自动</el-checkbox>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">全局配置</label>
+      <div class="content">
+
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">项目配置</label>
+      <div class="content"></div>
+    </div>
+    <div class="line">
+      <label class="title">分享成员</label>
+      <div class="content">
+        <el-select v-model="member" multiple placeholder="请选择">
+          <el-option
+              v-for="item in userList"
+              :key="item.id"
+              :label="item.name"
+              :disabled="item.default"
+              :value="item.id">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">描述</label>
+      <div class="content">
+        <el-input
+            style="width: 350px;"
+            type="textarea"
+            :rows="3"
+            placeholder="请输入内容"
+            v-model="describe">
+        </el-input>
+      </div>
+    </div>
+    <div class="line">
+      <label class="title">完成后</label>
+      <div class="content"></div>
+    </div>
+    <div class="footer-action">
+      <el-button @click="onsubmit" type="primary">保存</el-button>
+      <el-button @click="oncancel">取消</el-button>
     </div>
   </div>
 </template>
@@ -25,12 +98,25 @@
 <script>
   import { mapState } from 'vuex'
   import { actions } from '../../store/constants'
+  import { routes } from '../../router/constants'
   export default {
     name: 'compileInfo',
     data() {
       return {
         name: '',
-        config: null
+        config: null,
+        version: null,
+        versionList: [],
+        publicTypes: ['私有部署', '常规迭代', '发布测试'],
+        type: '',
+        publishingMode: {
+          git: false,
+          download: false,
+          auto: true
+        },
+        userList: [],
+        member: null,
+        describe: ''
 
       }
     },
@@ -44,6 +130,16 @@
     beforeCreate() {
       this.$store.dispatch(actions.configs.queryConfigList)
     },
+    methods: {
+      onsubmit() {
+
+      },
+      oncancel() {
+        this.$router.replace({
+          name: routes.compile.list
+        })
+      }
+    }
   }
 </script>
 
