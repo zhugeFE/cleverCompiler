48ad6023ad709edb0ee081f931f4154827a4ea05
董永强 <yqdong@37degree.com>
2020-03-04 10:37:45 +0800

    update

--- webapp/src/store/reducer/user.ts
+++ webapp/src/store/reducer/user.ts
@@ -1,8 +1,9 @@
-import { Action } from "redux";
+import { Action } from "../actionTypes";
 import { userActions } from '../actionTypes'
 import defaultState, { RootState } from '../state'
 
 const user = function (state: RootState = defaultState, action: Action) {
+  let userState = Object.assign(defaultState.user)
   switch (action.type) {
     case userActions.UPDATE_CURRENT:
       return {
@@ -10,6 +11,11 @@ const user = function (state: RootState = defaultState, action: Action) {
           name: '李四'
         }
       }
+    case userActions.LOGGIN:
+      userState = Object.assign({
+        currentUser: action.value
+      }, userState)
+      return Object.assign(userState, defaultState)
     default:
       return state
   }
