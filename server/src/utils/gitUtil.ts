e029dde3340d932daae5295a7ef8746111a7f0a7
董永强 <yqdong@37degree.com>
2020-04-01 17:43:36 +0800

    update

--- server/src/utils/gitUtil.ts
+++ server/src/utils/gitUtil.ts
@@ -1,12 +1,16 @@
 import axios, { Method } from 'axios'
 import sysDao from '../dao/sys'
 import logger from './logger';
+import * as _ from 'lodash';
 
 class GitUtil {
   async ajax<T> (path: string, method: Method, params?: object): Promise<T> {
     const sysInfo = await sysDao.getSysInfo()
     const url = `api/v3/${path}`
     logger.info('gitAPI请求', url)
+    const param = _.assign({
+      'per_page': 100
+    }, params || {})
     const res = await axios({
       url,
       method,
@@ -14,7 +18,7 @@ class GitUtil {
       headers: {
         'PRIVATE-TOKEN': sysInfo.gitToken
       },
-      [method.toLowerCase() === 'post' ? 'data' : 'params']: params
+      [method.toLowerCase() === 'post' ? 'data' : 'params']: param
     }) as {
       data: T;
     }
