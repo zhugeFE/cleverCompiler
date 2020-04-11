78d6b25c65f4bd2517467c2b2d5f0b6cee2bcac9
董永强 <yqdong@37degree.com>
2020-04-12 02:55:47 +0800

    update

--- webapp/src/modules/projectManage/edit/textConfig.tsx
+++ webapp/src/modules/projectManage/edit/textConfig.tsx
@@ -7,6 +7,7 @@ import api from '../../../store/api';
 import { ApiResult } from '../../../utils/ajax';
 import GitFileEditor from './fileEditor';
 import './styles/textConfig.less'
+import { FormInstance } from 'antd/lib/form';
 
 interface Props {
   gitId: string;
@@ -22,17 +23,25 @@ interface State {
   fileContent: string;
   formData: FormData;
   reg: RegExp;
+  displayContent: string;
 }
 class GitTextConfig extends React.Component<Props, State> {
+  form: React.RefObject<FormInstance> = React.createRef();
   constructor (props: Props) {
     super(props)
     this.state = {
       fileContent: '',
       formData: {},
-      reg: null
+      reg: null,
+      displayContent: ''
     }
     this.onSelectFile = this.onSelectFile.bind(this)
     this.onChange = this.onChange.bind(this)
+    this.onReplace = this.onReplace.bind(this)
+    this.onReset = this.onReset.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onCancel = this.onCancel.bind(this)
+    this.onBack = this.onBack.bind(this)
   }
   onSelectFile (filePath: string) {
     ajax({
@@ -44,7 +53,8 @@ class GitTextConfig extends React.Component<Props, State> {
     })
     .then((res: ApiResult<string>) => {
       this.setState({
-        fileContent: res.data
+        fileContent: res.data,
+        displayContent: res.data
       })
     })
     .catch(err => {
@@ -58,18 +68,50 @@ class GitTextConfig extends React.Component<Props, State> {
       reg: new RegExp(formData.reg, `${formData.global ? 'g' : ''}${formData.ignoreCase ? 'i' : ''}`)
     })
   }
+  onReplace () {
+    this.setState({
+      displayContent: this.state.displayContent.replace(this.state.reg, this.state.formData.value)
+    })
+  }
+  onReset () {
+    this.setState({
+      displayContent: this.state.fileContent
+    })
+  }
+  onSubmit () {
+    this.form.current.validateFields()
+    .then(() => {
+      console.log('表单没问题了')
+    })
+    .catch(() => {
+      console.error('表单校验失败')
+    })
+  }
+  onCancel () {
+    console.log('on cancel')
+  }
+  onBack () {
+    console.log('on back')
+  }
   render () {
     const layout = {
       labelCol: { span: 6 }
     }
-    const fileContent = this.state.fileContent
     return (
-      <Modal className="git-config-modal" visible={true} title={
-        <a><LeftOutlined style={{marginRight: '5px'}}/>切换类型</a>
-      } width="90%" okText="保存" cancelText="取消">
+      <Modal 
+        className="git-config-modal" 
+        visible={true} 
+        title={<a onClick={this.onBack}><LeftOutlined style={{marginRight: '5px'}}/>切换类型</a>} 
+        width="90%" 
+        okText="保存" 
+        cancelText="取消"
+        onCancel={this.onCancel}
+        onOk={this.onSubmit}>
         <FileTree onSelect={this.onSelectFile} gitId={this.props.gitId}></FileTree>
         <div className="git-cm-left-panel git-text-config">
-          <Form layout="inline" {...layout} onValuesChange={this.onChange}>
+          <Form ref={this.form}
+            layout="inline" {...layout} 
+            onValuesChange={this.onChange}>
             <Form.Item name="reg" label="匹配正则" className="long" 
               rules={[{
                 required: true,
@@ -83,13 +125,12 @@ class GitTextConfig extends React.Component<Props, State> {
             <Form.Item valuePropName="checked" name="ignoreCase">
               <Checkbox>忽略大小写</Checkbox>
             </Form.Item>
-            <Button type="primary">查找</Button>
-            <Button>还原</Button>
             <div className="form-divider"></div>
             <Form.Item label="替换为" name="value" className="long">
               <Input></Input>
             </Form.Item>
-            <Button type="primary">测试替换</Button>
+            <Button type="primary" onClick={this.onReplace}>替换</Button>
+            <Button onClick={this.onReset}>还原</Button>
             <div className="form-divider"></div>
             <Form.Item label="配置描述" name="desc" className="long" 
               rules={[{
@@ -99,7 +140,7 @@ class GitTextConfig extends React.Component<Props, State> {
               <Input></Input>
             </Form.Item>
           </Form>
-          <GitFileEditor reg={this.state.reg} content={fileContent}></GitFileEditor>
+          <GitFileEditor reg={this.state.reg} content={this.state.displayContent}></GitFileEditor>
         </div>
       </Modal>
     )
