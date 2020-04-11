d87bb6da8d85dd4493c077d68f20b6b8ad23db8f
董永强 <yqdong@37degree.com>
2020-04-12 02:26:15 +0800

    update

--- webapp/src/modules/projectManage/edit/textConfig.tsx
+++ webapp/src/modules/projectManage/edit/textConfig.tsx
@@ -11,16 +11,28 @@ import './styles/textConfig.less'
 interface Props {
   gitId: string;
 }
+interface FormData {
+  reg?: string;
+  value?: string;
+  desc?: string;
+  global?: boolean;
+  ignoreCase?: boolean;
+}
 interface State {
   fileContent: string;
+  formData: FormData;
+  reg: RegExp;
 }
 class GitTextConfig extends React.Component<Props, State> {
   constructor (props: Props) {
     super(props)
     this.state = {
-      fileContent: ''
+      fileContent: '',
+      formData: {},
+      reg: null
     }
     this.onSelectFile = this.onSelectFile.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
   onSelectFile (filePath: string) {
     ajax({
@@ -40,24 +52,35 @@ class GitTextConfig extends React.Component<Props, State> {
       console.error('文件读取失败', err)
     })
   }
+  onChange (changedValues: FormData, formData: FormData) {
+    this.setState({
+      formData,
+      reg: new RegExp(formData.reg, `${formData.global ? 'g' : ''}${formData.ignoreCase ? 'i' : ''}`)
+    })
+  }
   render () {
     const layout = {
       labelCol: { span: 6 }
     }
+    const fileContent = this.state.fileContent
     return (
       <Modal className="git-config-modal" visible={true} title={
         <a><LeftOutlined style={{marginRight: '5px'}}/>切换类型</a>
       } width="90%" okText="保存" cancelText="取消">
         <FileTree onSelect={this.onSelectFile} gitId={this.props.gitId}></FileTree>
         <div className="git-cm-left-panel git-text-config">
-          <Form layout="inline" {...layout}>
-            <Form.Item name="reg" label="匹配正则" className="long">
+          <Form layout="inline" {...layout} onValuesChange={this.onChange}>
+            <Form.Item name="reg" label="匹配正则" className="long" 
+              rules={[{
+                required: true,
+                message: '匹配规则不能为空'
+              }]}>
               <Input></Input>
             </Form.Item>
-            <Form.Item valuePropName="global">
+            <Form.Item valuePropName="checked" name="global">
               <Checkbox>全局</Checkbox>
             </Form.Item>
-            <Form.Item valuePropName="ignoreCase">
+            <Form.Item valuePropName="checked" name="ignoreCase">
               <Checkbox>忽略大小写</Checkbox>
             </Form.Item>
             <Button type="primary">查找</Button>
@@ -68,11 +91,15 @@ class GitTextConfig extends React.Component<Props, State> {
             </Form.Item>
             <Button type="primary">测试替换</Button>
             <div className="form-divider"></div>
-            <Form.Item label="配置描述" name="desc" className="long">
+            <Form.Item label="配置描述" name="desc" className="long" 
+              rules={[{
+                required: true,
+                message: '描述信息不能为空'
+              }]}>
               <Input></Input>
             </Form.Item>
           </Form>
-          <GitFileEditor content={this.state.fileContent}></GitFileEditor>
+          <GitFileEditor reg={this.state.reg} content={fileContent}></GitFileEditor>
         </div>
       </Modal>
     )
